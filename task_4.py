35b6b441c71f7eaa8fe8b9fd2d8381f5af759fae
Dmitriy <dmitrii0646@gmail.com>
2026-03-05 21:13:46 +0300

    style: исправить форматирование по PEP 8

--- task_4.py
+++ task_4.py
@@ -3,4 +3,4 @@ completed_tasks = ['task_002', 'task_012', 'task_006']
 
 completed_tasks.append(new_tasks.pop(4))
 new_tasks.remove('task_007')
-print('Следующая задача: ',new_tasks[-1])
+print('Следующая задача:', new_tasks[-1])
